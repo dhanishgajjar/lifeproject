92dd98f6d57228a03e074be9228598aa7454cf38
Dhanish <dhanish.b.gajjar@gmail.com>
2019-07-20 20:36:37 +0100

    updated readme

--- src/App.js
+++ src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+class App extends React.Component {
+  state = {
+    years: "90",
+    date: "1983-09-15",
+    total: ""
+  };
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  processDate = date => {
+    const processedDate = new Date(Date.parse(date));
+    return {
+      year: processedDate.getFullYear(),
+      month: processedDate.getMonth()
+    };
+  };
+
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const { year: birthYear, month: birthMonth } = this.processDate(
+      this.state.date
+    );
+    const { year: currentYear, month: currentMonth } = this.processDate(
+      new Date()
+    );
+
+    const total =
+      (currentYear - (birthYear + 1)) * 12 +
+      (12 - birthMonth) +
+      (currentMonth - 1);
+
+    this.setState({ total });
+  };
+
+  render() {
+    const { years, total } = this.state;
+    const months = [...Array(years * 12).keys()];
+
+    return (
+      <>
+        <div className="years">
+          {months.map(month => (
+            <div key={month} className={month < total ? `box fill` : `box`} />
+          ))}
+        </div>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="">Date of Birth</label>
+          <input
+            type="date"
+            name="date"
+            value={this.state.date}
+            onChange={this.handleChange}
+          />
+          <label htmlFor="years">Life Expectancy</label>
+          <input
+            id="years"
+            type="number"
+            name="years"
+            value={this.state.years}
+            onChange={this.handleChange}
+          />
+          <button type="submit">Show Life!</button>
+        </form>
+      </>
+    );
+  }
 }
 
 export default App;
